e51c25aaa6da783010107dfbcaf573d5eca7c59e
Lovelesh Sharma <lovelesh90@gmail.com>
2023-09-19 21:18:31 +0530

    Moves amount calculation logic from Rental class to Movie Class as Movie price should be associated with Movie class

--- src/main/java/com/thoughtworks/movierental/Movie.java
+++ src/main/java/com/thoughtworks/movierental/Movie.java
@@ -18,6 +18,30 @@ public class Movie {
         return priceCode;
     }
 
+    public double getPrice(int daysRented) {
+        double amount = 0;
+        switch (this.priceCode) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (daysRented > 2)
+                    amount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount += 1.5;
+                if (daysRented > 3)
+                    amount += (daysRented - 3) * 1.5;
+                break;
+            case Movie.BLUERAY:
+                amount += daysRented * 4;
+                break;
+        }
+
+        return amount;
+    }
+
     public void setPriceCode(int arg) {
         priceCode = arg;
     }
--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -18,26 +18,7 @@ public class Rental {
     }
 
     public double amount() {
-        double amount = 0;
-        switch (movie.getPriceCode()) {
-            case Movie.REGULAR:
-                amount += 2;
-                if (daysRented > 2)
-                    amount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                amount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                amount += 1.5;
-                if (daysRented > 3)
-                    amount += (daysRented - 3) * 1.5;
-                break;
-            case Movie.BLUERAY:
-                amount += daysRented * 4;
-                break;
-        }
-        return amount;
+        return this.movie.getPrice(this.daysRented);
     }
 
     public int frequentRenterPoints() {
